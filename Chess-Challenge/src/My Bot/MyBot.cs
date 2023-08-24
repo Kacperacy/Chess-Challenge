c3ade1e6f5b07a6f173b4914d27e518b494bdb5f
Kacper Maciołek <kacper.maciolek00@gmail.com>
2023-08-24 08:15:20 +0200

    Code cleanup, Move[] changed to Span<Move>

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,6 +3,14 @@ using System;
 
 public class MyBot : IChessBot
 {
+    // Pawn, Knight, Bishop, Rook, Queen, King 
+    int[] pieceVal = { 0, 100, 300, 310, 500, 900, 10000 };
+
+    #region TTEntry
+    // 0 = Invalid
+    // 1 = Upper bound
+    // 2 = Lower bound
+    // 3 = Exact score
     struct TTEntry {
         public ulong key;
         public Move move;
@@ -12,12 +20,14 @@ public class MyBot : IChessBot
         }
     }
 
-    private TTEntry[] tt = new TTEntry [0x400000];
+    TTEntry[] tt = new TTEntry [0x400000];
+    #endregion
     
-    int[] pieceVal = { 0, 100, 300, 310, 500, 900, 10000 };
-    int mateVal = 99999999;
-    Move bestMoveRoot;
+    // Mate value = 99999
+    Move moveRoot;
+    private readonly int[] moveScores = new int[218];
     
+    #region Evaluate()
     int Evaluate(Board board)
     {
         int sum = 0;
@@ -27,60 +37,72 @@ public class MyBot : IChessBot
 
         return sum * (board.IsWhiteToMove ? 1 : -1);
     }
+    #endregion
     
+    #region Search()
     int Search(Board board, Timer timer, int alpha, int beta, int depth, int ply)
     {
+        bool isCheck = board.IsInCheck(), isRoot = ply++ == 0;
+        
+        if (!isRoot && board.IsRepeatedPosition()) return 0;
+        
         ulong key = board.ZobristKey;
-        
-        if (ply > 0 && board.IsRepeatedPosition()) return 0;
-        
         TTEntry entry = tt[key & 0x3FFFFF];
         
-        if(ply > 0 && entry.key == key && entry.depth >= depth && (
+        // Transposition table lookup -> Found a valid entry for this position
+        // Avoid retrieving mate scores from the TT since they aren't accurate to the ply
+        if(!isRoot && entry.key == key && entry.depth >= depth && Math.Abs(entry.score) < 50000 && (
                entry.bound == 3 // exact score
                || entry.bound == 2 && entry.score >= beta // lower bound, fail high
                || entry.bound == 1 && entry.score <= alpha // upper bound, fail low
            )) return entry.score;
         
-        Move[] moves = board.GetLegalMoves();
+        // Check extension
+        if(isCheck)
+            depth++;
         
-        if (moves.Length == 0)
-            return board.IsInCheck() ? ply - mateVal : 0;
-
         if (depth == 0)
             return Evaluate(board);
-        
-        int[] scores = new int[moves.Length];
-        
-        for(int i = 0; i < moves.Length; i++) {
-            Move move = moves[i];
-            if(move == entry.move) scores[i] = mateVal;
-        }
 
-        int bestEval = int.MinValue;
+        Span<Move> moveSpan = stackalloc Move[218];
+        board.GetLegalMovesNonAlloc(ref moveSpan);
+        
+        if (moveSpan.IsEmpty)
+            return isCheck ? ply - 99999 : 0;
+
+        int movesScored = 0;
+
+        // Order moves in reverse order -> negative values are ordered higher hence the flipped values
+        foreach (var move in moveSpan)
+            moveScores[movesScored++] = -(
+                // Stored move
+                move == entry.move ? 9_000_000 :
+                // MVV LVA
+                move.IsCapture ? 1_000_000 * (int)move.CapturePieceType - (int)move.MovePieceType :
+                // Return 0
+                0
+            );
+
+        moveScores.AsSpan(0, moveSpan.Length).Sort(moveSpan);
+        
+        int bestEval = int.MinValue, oldAlpha = alpha;
         Move bestMove = Move.NullMove;
-        int oldAlpha = alpha;
 
-        for(int i = 0; i < moves.Length; i++)
+        for(int i = 0; i < moveSpan.Length; i++)
         {
-            for(int j = i + 1; j < moves.Length; j++) {
-                if(scores[j] > scores[i])
-                    (scores[i], scores[j], moves[i], moves[j]) = (scores[j], scores[i], moves[j], moves[i]);
-            }
-            
-            Move move = moves[i];
+            Move move = moveSpan[i];
             board.MakeMove(move);
-            int evaluation = -Search(board, timer, -beta, -alpha, depth - 1, ply + 1);
+            int evaluation = -Search(board, timer, -beta, -alpha, depth - 1, ply);
             board.UndoMove(move);
             
-            if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) return mateVal;
+            if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) return 99999;
             
             if (evaluation > bestEval)
             {
                 bestEval = evaluation;
                 bestMove = move;
                 if (ply == 0)
-                    bestMoveRoot = move;
+                    moveRoot = move;
             }
             alpha = Math.Max(alpha, bestEval);
             if (alpha >= beta) break;
@@ -90,16 +112,18 @@ public class MyBot : IChessBot
 
         return bestEval;
     }
+    #endregion
 
+    #region Think()
     public Move Think(Board board, Timer timer)
     {
-        bestMoveRoot = Move.NullMove;
         for (int depth = 1; depth <= 50; depth++)
         {
-            Search(board, timer, -mateVal, mateVal, depth, 0);
+            Search(board, timer, -99999, 99999, depth, 0);
 
             if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) break;
         }
-        return bestMoveRoot.IsNull ? board.GetLegalMoves()[0] : bestMoveRoot;
+        return moveRoot.IsNull ? board.GetLegalMoves()[0] : moveRoot;
     }
+    #endregion
 }
